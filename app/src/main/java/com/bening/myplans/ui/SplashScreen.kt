3ae12513382d782c5d80e42197c8eb896feb6124
safrudin <safrudin2443420@gmail.com>
2021-11-01 10:22:00 +0700

    initial commit

---
+++ app/src/main/java/com/bening/myplans/ui/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.bening.myplans.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bening.myplans.R
+import com.bening.myplans.main.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }, 2000L)
+    }
+}
