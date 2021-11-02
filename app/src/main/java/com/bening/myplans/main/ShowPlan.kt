9af8804f8d1f24ec30508103e0c428339beac5ba
safrudin <safrudin2443420@gmail.com>
2021-11-02 09:23:31 +0700

    repair tvTitle size

---
+++ app/src/main/java/com/bening/myplans/main/ShowPlan.kt
@@ -0,0 +1,11 @@
+package com.bening.myplans.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowPlan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_plan)
+    }
+}
